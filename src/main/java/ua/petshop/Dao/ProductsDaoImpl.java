a5ef1aa151066a35ef8c03b6533c7610e102b956
Denys Sheptytskyi <neoxesis@mail.ru>
2017-04-22 20:43:13 +0300

    fixed session management

--- src/main/java/ua/petshop/Dao/ProductsDaoImpl.java
+++ src/main/java/ua/petshop/Dao/ProductsDaoImpl.java
@@ -19,30 +19,30 @@ public class ProductsDaoImpl implements ProductsDao {
     }
 
     public void addProduct(Product product) {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
         session.persist(product);
     }
 
     public List<Product> getAll() {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
         List<Product> list = session.createQuery("from Product").list();
         return list;
     }
 
     public Product getById(long id) {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
         Product product = session.get(Product.class, id);
         return product;
     }
 
     public void deleteProduct(long id) {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
         Product product = getById(id);
         session.delete(product);
     }
 
     public void updateProduct(Product product) {
-        Session session = sessionFactory.openSession();
+        Session session = sessionFactory.getCurrentSession();
         session.update(product);
     }
 }
